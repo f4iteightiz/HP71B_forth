fe1c2f875a70cb652cd81dfa0d0c2642101170dc
f4iteightiz <38010488+f4iteightiz@users.noreply.github.com>
2025-05-07 20:53:43 +0200

    Add files via upload

---
+++ asmtoDAT_EPER.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+grep -v '^*' /home/pascal/programming/hp71b/ASM/EPER.ASM | cut -c1-23 | sed 's/[[:space:]]\+$//' > /home/pascal/programming/hp71b/ASM/EPER.TXT
+unix2dos -v /home/pascal/programming/hp71b/ASM/EPER.TXT
+wine '/home/pascal/.wine/drive_c/Program Files (x86)/HP-Emulators/alifhdr32/alifhdr.exe' EPER.TXT EPER.DAT /T
+#
+
---
+++ asmtoDAT_GAMMA.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+grep -v '^*' /home/pascal/programming/hp71b/ASM/GAMMA.ASM | cut -c1-23 | sed 's/[[:space:]]\+$//' > /home/pascal/programming/hp71b/ASM/GAMMA.TXT
+unix2dos -v /home/pascal/programming/hp71b/ASM/GAMMA.TXT
+wine '/home/pascal/.wine/drive_c/Program Files (x86)/HP-Emulators/alifhdr32/alifhdr.exe' GAMMA.TXT GAMMA.DAT /T
+#
+
