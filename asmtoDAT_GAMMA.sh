35d5328b099fe9eca735a99deeb530c22d4fe591
f4iteightiz <38010488+f4iteightiz@users.noreply.github.com>
2025-07-19 18:27:22 +0200

    update

--- asmtoDAT_GAMMA.sh
+++ asmtoDAT_GAMMA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-grep -v '^*' /home/pascal/programming/hp71b/ASM/GAMMA.ASM | cut -c1-29 | sed 's/[[:space:]]\+$//' > /home/pascal/programming/hp71b/ASM/GAMMA.TXT
+grep -v '^*' /home/pascal/programming/hp71b/ASM/GAMMA.ASM | cut -c1-31 | sed 's/[[:space:]]\+$//' > /home/pascal/programming/hp71b/ASM/GAMMA.TXT
 unix2dos -v /home/pascal/programming/hp71b/ASM/GAMMA.TXT
 wine '/home/pascal/.wine/drive_c/Program Files (x86)/HP-Emulators/alifhdr32/alifhdr.exe' GAMMA.TXT GAMMA.DAT /T
 #
