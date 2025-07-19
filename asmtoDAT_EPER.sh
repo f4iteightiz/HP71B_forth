1876d3b5b5393757269b8e417ea1cd12c2d84091
f4iteightiz <38010488+f4iteightiz@users.noreply.github.com>
2025-07-19 13:50:58 +0200

    Update asmtoDAT_EPER.sh

--- asmtoDAT_EPER.sh
+++ asmtoDAT_EPER.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-grep -v '^*' /home/pascal/programming/hp71b/ASM/EPER.ASM | cut -c1-23 | sed 's/[[:space:]]\+$//' > /home/pascal/programming/hp71b/ASM/EPER.TXT
+grep -v '^*' /home/pascal/programming/hp71b/ASM/EPER.ASM | cut -c1-29 | sed 's/[[:space:]]\+$//' > /home/pascal/programming/hp71b/ASM/EPER.TXT
 unix2dos -v /home/pascal/programming/hp71b/ASM/EPER.TXT
 wine '/home/pascal/.wine/drive_c/Program Files (x86)/HP-Emulators/alifhdr32/alifhdr.exe' EPER.TXT EPER.DAT /T
 #
