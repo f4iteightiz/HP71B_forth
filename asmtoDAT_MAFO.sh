abc5c6f022ae508d39d8bbd86181bf6b1f8a39ce
f4iteightiz <38010488+f4iteightiz@users.noreply.github.com>
2025-07-19 13:50:22 +0200

    Update asmtoDAT_MAFO.sh

--- asmtoDAT_MAFO.sh
+++ asmtoDAT_MAFO.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # awk -f hp71basfilter MAFO.TXT > MAFO.TXA
-grep -v '^*' /home/user/programming/hp71b/ASM/MAFO.ASM | cut -c1-23 | sed 's/[[:space:]]\+$//' > /home/user/programming/hp71b/ASM/MAFO.TXT
+grep -v '^*' /home/user/programming/hp71b/ASM/MAFO.ASM | cut -c1-29 | sed 's/[[:space:]]\+$//' > /home/user/programming/hp71b/ASM/MAFO.TXT
 unix2dos -v /home/user/programming/hp71b/ASM/MAFO.TXT
 wine '/home/user/.wine/drive_c/Program Files (x86)/HP-Emulators/alifhdr32/alifhdr.exe' MAFO.TXT MAFO.DAT /T
 #
